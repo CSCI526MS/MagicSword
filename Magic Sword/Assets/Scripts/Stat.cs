ae0fdf601020882a3a47ba79d5e53ee39d5d88a3
zhleonxu <zhleonxu@gmail.com>
2018-09-18 21:29:58 -0700

    Health bar

---
+++ Magic Sword/Assets/Scripts/Stat.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class Stat {
+
+    [SerializeField]
+    private HealthBar hearlthBar;
+
+    private float hpValue;
+    private float hpMaxValue;
+
+
+
+    // Use this for initialization
+    void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public float CurrentHP
+    {
+        get
+        {
+            return hpValue;
+        }
+
+        set
+        {
+            hpValue = value;
+            hearlthBar.Value = value;
+            
+        }
+    }
+
+    public float MaxHP
+    {
+        get
+        {
+            return hpMaxValue;
+        }
+
+        set
+        {
+            hpMaxValue = value;
+            hearlthBar.MaxValue = value;
+        }
+    }
+
+}

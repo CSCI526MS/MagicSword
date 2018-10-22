93bea38b7243e55167b232ecf0e59af7902911b1
zhleonxu <zhleonxu@gmail.com>
2018-10-22 16:45:09 -0700

    bug fixed

---
+++ Magic Sword/Assets/Scripts/EquippableItem.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public enum EquipmentType
+{
+	Consume,
+	Helmet,
+	Weapon
+}
+
+[CreateAssetMenu]
+public class EquippableItem : Item {
+
+	public EquipmentType EquipmentType;
+    /*
+	public EquippableItem(string itemId, EquipmentType EquipmentType, int hp, int speed, int attack, int defense, Sprite icon) {
+		this.itemId = itemId;
+		this.EquipmentType = EquipmentType;
+        this.properties[0] = hp;
+        this.properties[1] = speed;
+        this.properties[2] = attack;
+        this.properties[3] = defense;
+        this.icon = icon;
+	}
+    */
+	public void Equip(InventoryUI c)
+	{
+		//Debug.Log("equip " + " ??? here ?");
+	}
+
+	public void Unequip(InventoryUI c)
+	{
+		Debug.Log("unequip ");
+    }
+}
---
+++ Magic Sword/Assets/Scripts/Inventory.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+public class Inventory : MonoBehaviour {
+
+	// Use this for initialization
+	// itemList
+    public List<Item> itemList = new List<Item>();
+    [SerializeField] public InventorySlot[] inventorySlots;
+    [SerializeField] GameObject itemsParent;
+
+
+	// public event Action<Item> OnItemRightClickedEvent;
+    //
+    // private void Awake() {
+    //     Debug.Log("hll");
+	// 	for (int i = 0; i < inventorySlots.Length; i++) {
+    //         Debug.Log("inside for loop");
+	// 		inventorySlots[i].OnRightClickEvent += OnItemRightClickedEvent;
+	// 	}
+	// }
+
+	private int sizeLimit = 18;
+    public bool addItem (EquippableItem item) {
+		// Debug.Log("item put in the bag");
+        Debug.Log("item.EquipmentType = " + item.EquipmentType);
+        Debug.Log("item.hp = " + item.properties[0]);
+        Debug.Log("item.itemId = " + item.itemId);
+        for (int i = 0; i < inventorySlots.Length; i++) {
+            Debug.Log(inventorySlots[i].Item);
+            if (inventorySlots[i].Item == null) {
+                inventorySlots[i].Item = item;
+                // itemList.Add(item);
+                return true;
+            }
+        }
+
+        return false;
+	}
+
+    // private void setStat(EquippableItem item, float hp, float speed, float attack, float defense) {
+    //
+    //     item.hp = hp;
+    //     item.speed = speed;
+    //     item.attack = attack;
+    //     item.defense = defense;
+    // }
+
+
+    public bool IsFull() {
+        for (int i = 0; i < inventorySlots.Length; i++) {
+            if (inventorySlots[i].Item == null) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public bool removeItem (Item item) {
+
+        for (int i = 0; i < inventorySlots.Length; i++) {
+            if (inventorySlots[i].Item == item) {
+                inventorySlots[i].Item = null;
+                // itemList.Remove(item);
+                return true;
+            }
+        }
+
+        return false;
+	}
+
+    public List<Item> getItemList() {
+		return itemList;
+	}
+
+    private void OnValidate() {
+        Debug.Log("itemsParent ");
+        if (itemsParent != null) {
+
+            Debug.Log("inventorySlots ");
+            // itemsParent = GameObject.FindWithTag("InventoryPanel");
+            inventorySlots = itemsParent.GetComponentsInChildren<InventorySlot>();
+
+        }
+
+        RefreshUI();
+    }
+
+    private void RefreshUI() {
+        int i = 0;
+		for (; i < itemList.Count && i < inventorySlots.Length; i++) {
+			inventorySlots[i].Item = itemList[i];
+		}
+
+		for (; i < inventorySlots.Length; i++) {
+			inventorySlots[i].Item = null;
+		}
+    }
+}

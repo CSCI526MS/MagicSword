21d9fc5769093cc16f97a703e5b903a1878537c9
1angJ6 <358526394@qq.com>
2018-11-09 18:28:41 -0800

    Add boss skills

--- Magic Sword/Assets/Scripts/Enemy/Boss.cs
+++ Magic Sword/Assets/Scripts/Enemy/Boss.cs
@@ -12,6 +12,9 @@ public class Boss : MonoBehaviour {
 
     bool move;
 
+    // skills
+    public GameObject fireBall;
+
     public BossHealthBar healthBar;
     int health;
 	// Use this for initialization
@@ -21,8 +24,9 @@ public class Boss : MonoBehaviour {
         maxX = GameObject.Find("BottomRight").transform.position.x;
         minY = GameObject.Find("BottomRight").transform.position.y;
         maxY = GameObject.Find("TopLeft").transform.position.y;
-        
+
         //SummonMinions();
+        FireBall();
     }
 	
 	// Update is called once per frame
@@ -49,6 +53,36 @@ public class Boss : MonoBehaviour {
             GameObject newSlime = Instantiate(slime) as GameObject;
             newSlime.transform.position = new Vector2(randomX, randomY + 20);
         }
+    }
+
+    private void FireBall() {
+        float dia = Mathf.Sqrt(2) / 2;
+        float velocity = 4f;
+
+        GameObject clone1 = Instantiate(fireBall, gameObject.transform.position, gameObject.transform.rotation);
+        GameObject clone2 = Instantiate(fireBall, gameObject.transform.position, gameObject.transform.rotation);
+        GameObject clone3 = Instantiate(fireBall, gameObject.transform.position, gameObject.transform.rotation);
+        GameObject clone4 = Instantiate(fireBall, gameObject.transform.position, gameObject.transform.rotation);
+        GameObject clone5 = Instantiate(fireBall, gameObject.transform.position, gameObject.transform.rotation);
+        GameObject clone6 = Instantiate(fireBall, gameObject.transform.position, gameObject.transform.rotation);
+        GameObject clone7 = Instantiate(fireBall, gameObject.transform.position, gameObject.transform.rotation);
+        GameObject clone8 = Instantiate(fireBall, gameObject.transform.position, gameObject.transform.rotation);
+        clone1.transform.rotation = Quaternion.Euler(0f, 0f, 90);
+        clone2.transform.rotation = Quaternion.Euler(0f, 0f, 135);
+        clone3.transform.rotation = Quaternion.Euler(0f, 0f, 180);
+        clone4.transform.rotation = Quaternion.Euler(0f, 0f, 225);
+        clone5.transform.rotation = Quaternion.Euler(0f, 0f, 270);
+        clone6.transform.rotation = Quaternion.Euler(0f, 0f, 315);
+        clone7.transform.rotation = Quaternion.Euler(0f, 0f, 0);
+        clone8.transform.rotation = Quaternion.Euler(0f, 0f, 45);
+        clone1.GetComponent<Rigidbody2D>().velocity = new Vector2(0f, -1f) * velocity;
+        clone2.GetComponent<Rigidbody2D>().velocity = new Vector2(dia, -dia) * velocity;
+        clone3.GetComponent<Rigidbody2D>().velocity = new Vector2(1f, 0f) * velocity;
+        clone4.GetComponent<Rigidbody2D>().velocity = new Vector2(dia, dia) * velocity;
+        clone5.GetComponent<Rigidbody2D>().velocity = new Vector2(0, 1f) * velocity;
+        clone6.GetComponent<Rigidbody2D>().velocity = new Vector2(-dia, dia) * velocity;
+        clone7.GetComponent<Rigidbody2D>().velocity = new Vector2(-1f, 0f) * velocity;
+        clone8.GetComponent<Rigidbody2D>().velocity = new Vector2(-dia, -dia) * velocity;
     }
 
     public void TakeDamage(int damage){

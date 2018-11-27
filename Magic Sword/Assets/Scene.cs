68632d32837363385bf657d356253bfdb22ac831
Jiarui Gu <raygu612@gmail.com>
2018-11-27 14:32:27 -0800

    Gate Bug Fixes

--- Magic Sword/Assets/Scene.cs
+++ Magic Sword/Assets/Scene.cs
@@ -6,17 +6,20 @@ using UnityEngine.SceneManagement;
 public class Scene : MonoBehaviour {
 	void Start () {
         int level;
+        string gateName;
         switch (SceneManager.GetActiveScene().name)
         {
-            case "LevelOne": level = 0; break;
-            case "LevelTwo": level = 1; break;
-            case "LevelThree": level = 2; break;
-            default: level = 0; break;
+            case "LevelOne": level = 0; gateName = "Gate_1"; break;
+            case "LevelTwo": level = 1; gateName = "Gate_2"; break;
+            case "LevelThree": level = 2; gateName = "Gate_3"; break;
+            default: level = 0; gateName = ""; break;
         }
         DataLoader dataLoader = FindObjectOfType<DataLoader>();
         if (dataLoader.gameData.keyStatus[level])
         {
-            Destroy(GameObject.FindWithTag("Gate"));
+            GameObject gate = GameObject.Find(gateName);
+            gate.GetComponent<SpriteRenderer>().enabled = false;
+            gate.GetComponent<BoxCollider2D>().enabled = false;
         }
     }
 }
